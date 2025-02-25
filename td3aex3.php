8a2ec36e98a78aa4251c2a080741a059c8c6a79b
Babouchka473 <edouard.reucherand@proton.me>
2025-02-25 20:05:08 +0100

    Add files via upload

--- td3aex3.php
+++ td3aex3.php
@@ -2,16 +2,16 @@
 
 $tableau = array("pomme", "banane", "pomme", "orange", "banane", "pomme", "kiwi", "orange");
 
-echo "Éléments du tableau d'origine :\n";
+echo "Éléments du tableau d'origine : <br>";
 print_r($tableau);
 
 $occurrences = array_count_values($tableau);
 
-echo "\nNombre d'occurrences des valeurs du tableau :\n";
+echo "<br>Nombre d'occurrences des valeurs du tableau :<br>";
 print_r($occurrences);
 
 $valeurs_uniques = array_keys($occurrences);
 
-echo "\nValeurs uniques présentes dans le tableau d'origine :\n";
+echo "<br>Valeurs uniques présentes dans le tableau d'origine :<br>";
 print_r($valeurs_uniques);
 ?>
