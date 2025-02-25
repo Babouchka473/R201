cc3b146c738e8bc8812377c087ec4e7697b516ed
Babouchka473 <edouard.reucherand@proton.me>
2025-02-25 20:33:27 +0100

    Add files via upload

--- td3aex8.php
+++ td3aex8.php
@@ -1,9 +1,10 @@
 <?php
 $valeur = 9;
-
+echo $valeur
 $puissance = pow($valeur, 2);
-
+echo $puissance
 $valeur_absolue = abs($valeur);
-
+echo $valeur_absolue
 $racine_carree = sqrt($valeur_absolue); 
+echo $racine_carree
 ?>
