b7b7845f93d4bec02dae3be8c8a6115ccccd35a3
Babouchka473 <edouard.reucherand@proton.me>
2025-02-25 20:18:22 +0100

    Add files via upload

---
+++ td3aex7.php
@@ -0,0 +1,18 @@
+<?php
+$tableau = array(
+    "jean" => "bonbeur",
+    "jade" => "ormanger",
+    "alex" => "terieur",
+    "justin" => "ptitpeu"
+);
+
+$element = "alex";
+
+$cle_trouvee = array_search($element, $tableau);
+
+if ($cle_trouvee !== false) {
+    echo "La clé de l'élément '$element' est : $cle_trouvee <br>";
+} else {
+    echo "L'élément '$element' n'a pas été trouvé dans le tableau. <br>";
+}
+?>
